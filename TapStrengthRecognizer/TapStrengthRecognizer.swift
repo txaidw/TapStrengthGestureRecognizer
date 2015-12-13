172082a9887391197255c3a586c6c6273d0c803a
Txai Wieser <txaidw@gmail.com>
2015-12-13 14:37:56 -0200

    Recognizer class

---
+++ TapStrengthRecognizer/TapStrengthRecognizer.swift
@@ -0,0 +1,13 @@
+//
+//  TapStrengthRecognizer.swift
+//  TapStrengthRecognizer
+//
+//  Created by Txai Wieser on 13/12/15.
+//  Copyright © 2015 Txai Wieser. All rights reserved.
+//
+
+import UIKit
+
+class TapStrengthRecognizer: UIGestureRecognizer {
+
+}
--- TapStrengthRecognizer/ViewController.swift
+++ TapStrengthRecognizer/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var body: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
