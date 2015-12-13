2a9032668737203a8dadc62c5cf40058847d55ed
Txai Wieser <txaidw@gmail.com>
2015-12-13 01:29:33 -0200

    Initial Commit

---
+++ TapStrengthRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapStrengthRecognizer
+//
+//  Created by Txai Wieser on 13/12/15.
+//  Copyright © 2015 Txai Wieser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
